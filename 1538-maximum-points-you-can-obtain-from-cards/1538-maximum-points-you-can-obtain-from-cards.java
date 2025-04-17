03bf6181cbef3d300a853355cf2fc2a9c84c300b
saathvika123 <kasuvojulasaathvika@gmail.com>
2025-04-17 15:38:20 +0530

    Time: 3 ms (33.14%), Space: 55.1 MB (64.04%) - LeetHub

--- 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.java
+++ 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.java
@@ -1,30 +1,23 @@
 class Solution {
     public int maxScore(int[] a, int k) {
-        int n = a.length;
-        int totalSum = 0;
-
-        for (int num : a) {
-            totalSum += num;
-        }
-
-        // Find the minimum sum of subarray of length n - k
-        int windowSum = 0, minSubarraySum = Integer.MAX_VALUE;
-        int l = 0;
-
+        int n=a.length;
+        int s=0;
+        for(int i:a) 
+            s+=i;
+        int windowSum = 0,minSubarraySum = Integer.MAX_VALUE;
+        int l=0;
         for (int r = 0; r < n; r++) {
             windowSum += a[r];
-
-            // When window size exceeds n - k
-            if (r - l + 1 > n - k) {
+                    if (r - l == n - k) {
                 windowSum -= a[l];
                 l++;
             }
 
             if (r - l + 1 == n - k) {
-                minSubarraySum = Math.min(minSubarraySum, windowSum);
+                minSubarraySum=Math.min(minSubarraySum, windowSum);
             }
         }
 
-        return totalSum - (n == k ? 0 : minSubarraySum);
+        return s-(n==k?0:minSubarraySum);
     }
 }
