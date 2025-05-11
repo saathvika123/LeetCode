e563623b881f1377e6e1fe0fbd148cdd313dbac9
saathvika123 <kasuvojulasaathvika@gmail.com>
2025-05-11 22:16:37 +0530

    Time: 43 ms (9.47%), Space: 44.3 MB (100%) - LeetHub

--- 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.java
+++ 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.java
@@ -3,26 +3,20 @@ class Solution {
         int r=a.length;
         for(int i=0;i<r;i++)
         {
-            Map<Integer,Integer> mp=new TreeMap<>();
+            Map<Integer,Integer> mp=new HashMap<>();
+            Map<Integer,Integer> mp1=new HashMap<>();
+
             for(int j=0;j<r;j++)
             {
                 mp.put(a[i][j],mp.getOrDefault(a[i][j],0)+1);
-            }
-            
-                if(mp.size()!=r) return false;
-            
-        }
-        for(int i=0;i<r;i++)
-        {
-            Map<Integer,Integer> mp1=new TreeMap<>();
-            for(int j=0;j<r;j++)
-            {
                 mp1.put(a[j][i],mp1.getOrDefault(a[j][i],0)+1);
+
             }
-          
-                if(mp1.size()!=r) return false;
+            
+                if(mp.size()!=r || mp1.size()!=r) return false;
             
         }
+        
         return true;
     }
 }
