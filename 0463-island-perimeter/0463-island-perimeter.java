ee949f30f23183fae76ad156f982f0bc245668e4
saathvika123 <kasuvojulasaathvika@gmail.com>
2025-05-29 11:32:46 +0530

    Time: 8 ms (20.75%), Space: 45.8 MB (59.91%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -11,28 +11,22 @@ class Solution {
             {
                 if(a[i][j]==1)
                 {
-                    sr=i;
-                    sc=j;
-                    break;
+                   return bfs(a,i,j,r,c,vis);
                 }
             }
         }
-        count = 0;
-        if(sr==-1) return 0;
-        return bfs(a,sr,sc,r,c,vis);
+       return 0;
+        
     }
     public static int bfs(int a[][],int sr,int sc,int r,int c,int vis[][])
     {   if(sr < 0 || sr>=r || sc<0 || sc>=c || a[sr][sc]==0)
           return 1;
           if(vis[sr][sc]==1) return 0;
           vis[sr][sc]=1;
-        // if(a[sr][sc]==1)
-        // {
         count = bfs(a,sr - 1, sc,r,c,vis) +
          bfs(a, sr, sc + 1,r,c,vis) + 
          bfs(a, sr + 1, sc,r,c,vis) +
           bfs(a, sr, sc - 1,r,c,vis);
-        // }
         return count;
     }
 }
