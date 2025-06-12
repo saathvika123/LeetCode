cb90f3d7ef393f54889606fda89c64d7a442c506
saathvika123 <kasuvojulasaathvika@gmail.com>
2025-06-12 17:37:28 +0530

    Time: 3 ms (31.65%), Space: 44.8 MB (42.14%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,39 +1,57 @@
 class Solution {
     static int di[]={-1,0,1,0};
     static int dj[]={0,1,0,-1};
-    public int maxAreaOfIsland(int[][] grid) {
-    int mx=0;
-        int sr=0,sc=0,r=grid.length,c=grid[0].length,count=0;
-        boolean vis[][]=new boolean[r][c];
-        for(int i=0;i<grid.length;i++)
+    public int maxAreaOfIsland(int[][] grid) 
+    {
+        int r=grid.length,c=grid[0].length;
+        int mx=0;
+        for(int i=0;i<r;i++)
         {
-            for(int j=0;j<grid[0].length;j++)
+            for(int j=0;j<c;j++)
             {
                 if(grid[i][j]==1)
                 {
-                    int val=bfs(grid,r,c,i,j,vis);
+                    int val=bfs(grid,i,j,r,c,8);
                     mx=Math.max(mx,val);
-
                 }
             }
         }
         return mx;
     }
-
-    public int bfs(int a[][],int r,int c,int sr,int sc,boolean vis[][])
+    public int bfs(int grid[][],int sr,int sc,int r,int c,int nw)
     {
-        int ans=1;
-        if(sr<0 || sr>=r || sc<0 || sc>=c || a[sr][sc]==0 || vis[sr][sc])
+        int cnt=1;
+        Queue<Pair> q=new LinkedList<>();
+        q.add(new Pair(sr,sc));
+        grid[sr][sc]=nw;
+        int i,j;
+        while(!q.isEmpty())
         {
-            return 0;
+            Pair s=q.remove();
+            i=s.i;
+            j=s.j;
+            for(int k=0;k<4;k++)
+            {
+                int ni=i+di[k];
+                int nj=j+dj[k];
+                if(ni>=0 && ni<r && nj>=0 && nj<c && grid[ni][nj]==1)
+                {
+                    q.add(new Pair(ni,nj));
+                    grid[ni][nj]=nw;
+                    cnt++;
+                }
+
+            }
         }
-        vis[sr][sc]=true;
-        for(int k=0;k<4;k++)
-        {
-            int ni=di[k]+sr;
-            int nj=dj[k]+sc;
-            ans+=bfs(a,r,c,ni,nj,vis);
-        }
-        return ans;
+        return cnt;
+
+    }
+}
+class Pair
+{
+    int i,j;
+    Pair(int i,int j){
+        this.i=i;
+        this.j=j;
     }
 }
